aedc653a09ed7ee9972873ad21f5bf2d32cd5358
USERNAME <tuyennv.1995@gmail.com>
2022-11-06 23:37:53 +0700

    categories

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  mode: 'jit',
+  content: ["./index.html", "./src/**/*.{js,jsx}", "./public/script.js"],
   theme: {
     extend: {
       colors: {
@@ -8,7 +9,22 @@ module.exports = {
         paragrapText: "#787878",
         hoverLink: "#ff8f9c",
         colorTitleLinkMenu: '#454545',
-        colorTextLinkMenu: "#787878"
+        colorTextLinkMenu: "#787878",
+        colorHeading: "212121",
+        colorParagrap: "#787878",
+        colorHong: "#ff8f9c",
+        colorYellow: "#f6a355",
+        backgroundGray:'#ededed',
+      },
+      fontSize:{
+          "1":"1px",
+          "9":"9px",
+          "11":"11px",
+          "13":"13px",
+          "15":"15px",
+          "17":"17px",
+          "19":"19px",
+          "21":"21px",
       },
       screens: {
         xs: "480px",
@@ -16,6 +32,11 @@ module.exports = {
         md: "960px",
         lg: "1200px",
         xl: "1536px",
+      },
+      aspectRatio: {
+        '4/3': '4 / 3',
+        '2/1': '2 / 1',
+        '1/1': '1 / 1',
       },
     },
   },
